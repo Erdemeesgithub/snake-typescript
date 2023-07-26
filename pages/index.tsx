7fdac5ef65904671c51551531484cd45a281a4d4
Erdemeesgithub <berdemdelgerekh@gmail.com>
2023-07-26 14:22:45 +0800

    high score dutu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
-import { log } from "console";
+import { dir, log } from "console";
 import { useEffect, useState } from "react";
 import useInterval from "use-interval";
 import { useRouter } from "next/router";
+import { AiOutlineArrowUp } from "react-icons/ai";
+import { AiOutlineArrowDown } from "react-icons/ai";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import Stopwatch from "./stopwatch";
 
 const zoom = 20;
 const areaWidth = 30;
@@ -16,6 +21,8 @@ export default function Home() {
   const [direction, setDirection] = useState("down");
 
   const [food, setFood] = useState({ top: 5, left: 5 });
+
+  const [point, setPoint] = useState(0);
   const router = useRouter();
   const handleRefresh = () => {
     router.reload();
@@ -61,6 +68,10 @@ export default function Home() {
       });
     });
   }, []);
+
+  function addPoint() {
+    setPoint(point + 2);
+  }
 
   function generateFood() {
     const top = Math.floor(Math.random() * areaHeight);
@@ -132,12 +143,17 @@ export default function Home() {
         break;
       case "down":
         goDown();
+
         break;
+
       case "up":
         goUp();
+
         break;
+
       case "left":
         goLeft();
+
         break;
     }
 
@@ -146,15 +162,19 @@ export default function Home() {
       generateFood(); // generate new food
       if (direction == "up") {
         setBody([...body, { top: body[0].top + 1, left: body[0].left }]); //grow snake
+        addPoint();
       }
       if (direction == "down") {
         setBody([...body, { top: body[0].top - 1, left: body[0].left }]); //grow snake
+        addPoint();
       }
       if (direction == "right") {
         setBody([...body, { top: body[0].top, left: body[0].left + 1 }]); //grow snake
+        addPoint();
       }
       if (direction == "left") {
         setBody([...body, { top: body[0].top, left: body[0].left - 1 }]); //grow snake
+        addPoint();
       }
     }
     Gameover();
@@ -164,12 +184,17 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
+      <div>Points: {point}</div>
+      <Stopwatch />
       <div
         className="relative bg-slate-300"
         style={{
           width: areaWidth * zoom,
           height: areaHeight * zoom,
-          backgroundColor: "#808000",
+          backgroundImage:
+            "url(" +
+            "https://docimg.replit.com/images/tutorials/21-snake-kaboom/background.png" +
+            ")",
         }}
       >
         <div
@@ -189,12 +214,76 @@ export default function Home() {
               left: segment.left * zoom,
               width: zoom,
               height: zoom,
-              backgroundColor: "#355E3B",
+              backgroundColor: "#023020",
             }}
           ></div>
         ))}
       </div>
       <button onClick={handleRefresh}>Refresh</button>
+      <div
+        style={{
+          display: "flex",
+          gap: 0,
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          onClick={() => setDirection("up")}
+          style={{
+            width: 50,
+            height: 50,
+            borderWidth: 2,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <AiOutlineArrowUp />
+        </div>
+        <div style={{ display: "flex" }}>
+          <div
+            onClick={() => setDirection("left")}
+            style={{
+              width: 50,
+              height: 50,
+              borderWidth: 2,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <AiOutlineArrowLeft />
+          </div>
+          <div
+            onClick={() => setDirection("down")}
+            style={{
+              width: 50,
+              height: 50,
+              borderWidth: 2,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <AiOutlineArrowDown />
+          </div>
+          <div
+            onClick={() => setDirection("right")}
+            style={{
+              width: 50,
+              height: 50,
+              borderWidth: 2,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <AiOutlineArrowRight />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ pages/stopwatch.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(true);
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+  return (
+    <div className="stopwatch">
+      <div className="numbers">
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
